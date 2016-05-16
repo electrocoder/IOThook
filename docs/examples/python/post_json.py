799103b67349cf80e18c158a8300e02c8d3af601
electrocoder <electocoder@gmail.com>
2016-05-16 14:33:36 +0300

    python

---
+++ docs/examples/python/post_json.py
@@ -0,0 +1,26 @@
+
+# isi ve nem degerlerini python ile gonderme
+# 16.05.2016
+# electro
+
+import json
+import urllib
+import urllib2
+
+headers = {'content-type': 'application/json'}
+
+API_KEY = "11111"
+
+url = 'http://iothook.com/channel/api/API_KEY'
+
+data = {"temperature":"28.45","humidity":"8",}
+
+data = urllib.urlencode(data)
+
+req = urllib2.Request(url, data)
+response = urllib2.urlopen(req)
+
+html = response.read()
+
+print html
+
