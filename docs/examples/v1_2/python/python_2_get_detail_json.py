6827c755a56cf291bd5ed769fe799efa55f93645
IoThook <info@iothook.com>
2017-05-11 17:01:49 +0300

    apache license
    apache license

--- docs/examples/v1_2/python/python_2_get_detail_json.py
+++ docs/examples/v1_2/python/python_2_get_detail_json.py
@@ -22,7 +22,14 @@
   https://github.com/electrocoder/iotHook/issues
   sayfasindan veya Meşe Bilişim den yardım alabilirsiniz.
 
-  Yayin ve sahiplik http://mesebilisim.com
+  Yayin : http://mesebilisim.com
+  
+  Licensed under the Apache License, Version 2.0 (the "License").
+  You may not use this file except in compliance with the License.
+  A copy of the License is located at
+
+  http://www.apache.org/licenses/
+
 """
 
 import requests
